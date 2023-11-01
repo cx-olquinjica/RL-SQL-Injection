8743a3da809bb62adddec9c03c72c4f14d098d39
cx-olquinjica <aosiwaduo@outlook.com>
2023-11-01 11:52:08 +0800

    add initial page

--- public_html/index.php
+++ public_html/index.php
@@ -1,15 +1,36 @@
-<h1>Hello Cloudreach!</h1>
-<h4>Attempting MySQL connection from php...</h4>
 <?php
+echo "Welcome";
+echo "<br />";
+// These are the defined authentication environment in the db service
 $host = 'mysql';
 $user = 'root';
+// Database user password
 $pass = 'rootpassword';
-$conn = new mysqli($host, $user, $pass);
+// Database name
+$mydatabase = 'example';
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "Connected to MySQL successfully!";
+// Check the MySQL connection status
+$conn = new mysqli($host, $user, $pass);
+echo '<form action="index.php" method="post">';
+echo 'Name: <input type="text" name="name">';
+echo 'E-mail: <input type="text" name="email">';
+echo '<input type="submit">';
+echo '</form>';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $data = $_REQUEST['email'];
+    echo "<br />";
+    if ($result = $conn->query("SELECT name from customers WHERE surname = '$data' ")) {
+        // Dynamic query
+        echo "<br/>";
+        while ($row = mysqli_fetch_array($result)) {
+            echo "<b>Name:</b> " . $row['name'] . " ";
+            echo "<b>Company: </b>" . $row['company'] . "<br />";
+            echo "<b>Surname: </b>" . $row['surname'] . "<br />";
+        }
+        echo "Returned rows are: " . $result->num_rows;
+    }
 }
 
+$conn->close();
 ?>
